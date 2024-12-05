4b5ba0a58190b722d771bafe495914c374fc9cb9
Virgilio Di Falco <virgilio.difalco@gmail.com>
2024-12-05 15:39:59 +0100

    refactor: PageTemplateList updated

---
+++ src/ui/page-templates/list/PageTemplatePreview.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PageTemplateListMenuActions from 'ui/page-templates/list/PageTemplateListMenuActions';
+import PageConfigGrid from 'ui/pages/config/PageConfigGrid';
+import { getCellMap } from 'state/page-templates/helpers';
+
+const PageTemplatePreview = ({ item }) => {
+  const { code, descr, removePageTemplate } = item;
+  const cellMap = getCellMap(item);
+
+  return (
+    <div className="Page_Template_Preview_Container">
+      <div className="Page_Template_Preview_Content">
+        <div style={{ position: 'absolute', right: '10px' }}>
+          <PageTemplateListMenuActions
+            code={code}
+            onClickDelete={() => removePageTemplate(code)}
+          />
+        </div>
+        <div className="Page_Config_Grid_Container">
+          <PageConfigGrid cellMap={cellMap} />
+        </div>
+      </div>
+      {descr}
+    </div>
+  );
+};
+
+PageTemplatePreview.propTypes =
+{
+  item: PropTypes.shape({
+    code: PropTypes.string.isRequired,
+    descr: PropTypes.string.isRequired,
+    mainFrame: PropTypes.number.isRequired,
+    pluginCode: PropTypes.string,
+    template: PropTypes.string.isRequired,
+    extraProperties: PropTypes.string,
+    configuration: PropTypes.shape({
+      frames: PropTypes.shape({
+        pos: PropTypes.string.isRequired,
+        descr: PropTypes.string.isRequired,
+        defaultWidget: PropTypes.shape({
+          code: PropTypes.string.isRequired,
+        }).isRequired,
+        sketch: PropTypes.shape({
+          x1: PropTypes.number.isRequired,
+          x2: PropTypes.number.isRequired,
+          y1: PropTypes.number.isRequired,
+          y2: PropTypes.number.isRequired,
+        }),
+      }),
+    }).isRequired,
+    removePageTemplate: PropTypes.func.isRequired,
+  }),
+}.isRequired;
+
+export default PageTemplatePreview;
